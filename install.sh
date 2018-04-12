952a25d27ab7fdca8967c18cfff1027db17bf6e7
Daniel Johnson <daniel_johnson@selinc.com>
2018-04-12 16:40:47 -0700

    Created version 1 of my home folder config file tool. Running install backup the config files in the home directory and then creates symlinks to the config files within this repository. Running uninstall will restore the archived config files. Can be ran from this repo or the backup folder.

---
+++ install.sh
@@ -0,0 +1,20 @@
+# If a backup folder already exists, then ERROR
+if [ -d "$HOME/.dano-home-backup" ]; then
+   echo "ERROR: A backup folder already exists at \"$HOME/.dano-home-backup\". \
+         Run the uninstall script located there first." 
+   exit 1
+fi
+
+# Backup all affected files
+mkdir -p ~/.dano-home-backup
+mv $HOME/.bashrc $HOME/.bash_profile $HOME/.bash_aliases $HOME/.vimrc ~/.dano-home-backup 2> /dev/null
+
+# Create symbolic links to the config files specified in this repository.
+ln -s $PWD/config-files/bashrc $HOME/.bashrc
+ln -s $PWD/config-files/bash_profile $HOME/.bash_profile
+ln -s $PWD/config-files/bash_aliases $HOME/.bash_aliases
+ln -s $PWD/config-files/vimrc $HOME/.vimrc
+
+# Create a copy of the uninstall script in the backup directory in case someone
+# nukes this repo, but needs to recover.
+cp $PWD/uninstall.sh $HOME/.dano-home-backup
