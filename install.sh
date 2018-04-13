d77cf07111da5a73d067085029cad8660afd48c7
Daniel Johnson <daniel_johnson@selinc.com>
2018-04-13 16:06:18 -0700

    Began workin on bugfixes.
    Began quoting paramters now that I've learned how dangerous it is not to.

--- install.sh
+++ install.sh
@@ -7,14 +7,18 @@ fi
 
 # Backup all affected files
 mkdir -p ~/.dano-home-backup
-mv $HOME/.bashrc $HOME/.bash_profile $HOME/.bash_aliases $HOME/.vimrc ~/.dano-home-backup 2> /dev/null
+mv "$HOME/.bashrc" \
+   "$HOME/.bash_profile" \
+   "$HOME/.bash_aliases" \
+   "$HOME/.vimrc" \
+   '~/.dano-home-backup' 2> /dev/null
 
 # Create symbolic links to the config files specified in this repository.
-ln -s $PWD/config-files/bashrc $HOME/.bashrc
-ln -s $PWD/config-files/bash_profile $HOME/.bash_profile
-ln -s $PWD/config-files/bash_aliases $HOME/.bash_aliases
-ln -s $PWD/config-files/vimrc $HOME/.vimrc
+ln -s "$PWD/config-files/bashrc" "$HOME/.bashrc"
+ln -s "$PWD/config-files/bash_profile" "$HOME/.bash_profile"
+ln -s "$PWD/config-files/bash_aliases" "$HOME/.bash_aliases"
+ln -s "$PWD/config-files/vimrc" "$HOME/.vimrc"
 
 # Create a copy of the uninstall script in the backup directory in case someone
 # nukes this repo, but needs to recover.
-cp $PWD/uninstall.sh $HOME/.dano-home-backup
+cp "$PWD/uninstall.sh" "$HOME/.dano-home-backup"
--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,14 @@
 BACKUP_PATH="$HOME/.dano-home-backup"
 
 # Move config files back into place
-mv $BACKUP_PATH/.bashrc \
-   $BACKUP_PATH/.bash_profile \
-   $BACKUP_PATH/.bash_aliases \
-   $BACKUP_PATH/.vimrc \
-   $HOME/
+mv "$BACKUP_PATH/.bashrc" \
+   "$BACKUP_PATH/.bash_profile" \
+   "$BACKUP_PATH/.bash_aliases" \
+   "$BACKUP_PATH/.vimrc" \
+   "$HOME/"
 
 # Remove this script
-rm $BACKUP_PATH/uninstall.sh
+rm "$BACKUP_PATH/uninstall.sh"
 
 # Remove this directory
-rmdir $BACKUP_PATH
+rmdir "$BACKUP_PATH"
