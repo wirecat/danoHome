b05835a2693c85f0ac64db84459ee88983357e97
Daniel Johnson <djwirecat@gmail.com>
2018-04-28 11:22:07 -0700

    Added sway config to installer

--- install.sh
+++ install.sh
@@ -7,13 +7,14 @@ fi
 
 # Backup all affected files
 mkdir -p ~/.dano-home-backup
-mv $HOME/.bashrc $HOME/.bash_profile $HOME/.bash_aliases $HOME/.vimrc ~/.dano-home-backup 2> /dev/null
+mv $HOME/.bashrc $HOME/.bash_profile $HOME/.bash_aliases $HOME/.vimrc  "$XDG_CONFIG_HOME/sway/config" ~/.dano-home-backup 2> /dev/null
 
 # Create symbolic links to the config files specified in this repository.
 ln -s $PWD/config-files/bashrc $HOME/.bashrc
 ln -s $PWD/config-files/bash_profile $HOME/.bash_profile
 ln -s $PWD/config-files/bash_aliases $HOME/.bash_aliases
 ln -s $PWD/config-files/vimrc $HOME/.vimrc
+ln -s $PWD/config-files/sway "$XDG_CONFIG_HOME/sway/config"
 
 # Create a copy of the uninstall script in the backup directory in case someone
 # nukes this repo, but needs to recover.
