4cd852bbab1dbad077d7962aa09ef54016ed58a6
Daniel Johnson <daniel_johnson@selinc.com>
2018-04-30 11:24:37 -0700

    Fixed a couple bugs.
    Installer now correctly handles sway config.
    bahsrc no longer has hardcoded path to home directory. Addresses at least part of issues #2.

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ mv "$HOME/.bashrc" \
    "$HOME/.bash_aliases" \
    "$HOME/.vimrc" \
    "$BACKUP_PATH" 2> /dev/null
+mkdir -p "$XDG_CONFIG_HOME/sway"
 mv "$XDG_CONFIG_HOME/sway/config" "$BACKUP_PATH/sway" 2> /dev/null
 
 # Create symbolic links to the config files specified in this repository.
@@ -21,6 +22,7 @@ ln -s "$PWD/config-files/bashrc" "$HOME/.bashrc"
 ln -s "$PWD/config-files/bash_profile" "$HOME/.bash_profile"
 ln -s "$PWD/config-files/bash_aliases" "$HOME/.bash_aliases"
 ln -s "$PWD/config-files/vimrc" "$HOME/.vimrc"
+ln -s "$PWD/config-files/sway" "$XDG_CONFIG_HOME/sway/config"
 
 # Create a copy of the uninstall script in the backup directory in case someone
 # nukes this repo, but needs to recover.
