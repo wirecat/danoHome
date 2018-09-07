1c23286e2a6e9573b95f7f1810882cb0ca0a8d38
Daniel Johnson <daniel_johnson@selinc.com>
2018-09-07 16:33:02 -0700

    Updated vimrc configuration and removed obsolete ones.
    I no longer use i3status or sway so I have removed the configuration files
    for the time being. This also involved updating the install and uninstall
    scripts to no longer reference these configs.
    
    I fixed whitespace issues in my vimrc.
    
    I updated the vimrc to highlight trailing whitespaces and tabs by default.
    
    Updated the bash_profile so that it calls startx as soon as someone
    logs into TTY1. This will in turn kick of xorg-xinit and start up
    the X server.

--- install.sh
+++ install.sh
@@ -13,18 +13,13 @@ mv "$HOME/.bashrc" \
    "$HOME/.bash_profile" \
    "$HOME/.bash_aliases" \
    "$HOME/.vimrc" \
-   "$HOME/.i3status.conf" \
    "$BACKUP_PATH" 2> /dev/null
-mkdir -p "$XDG_CONFIG_HOME/sway"
-mv "$XDG_CONFIG_HOME/sway/config" "$BACKUP_PATH/sway" 2> /dev/null
 
 # Create symbolic links to the config files specified in this repository.
 ln -s "$PWD/config-files/bashrc" "$HOME/.bashrc"
 ln -s "$PWD/config-files/bash_profile" "$HOME/.bash_profile"
 ln -s "$PWD/config-files/bash_aliases" "$HOME/.bash_aliases"
 ln -s "$PWD/config-files/vimrc" "$HOME/.vimrc"
-ln -s "$PWD/config-files/sway" "$XDG_CONFIG_HOME/sway/config"
-ln -s "$PWD/config-files/i3status.conf" "$HOME/.i3status.conf"
 
 # Create a copy of the uninstall script in the backup directory in case someone
 # nukes this repo, but needs to recover.
--- uninstall.sh
+++ uninstall.sh
@@ -5,9 +5,7 @@ mv "$BACKUP_PATH/.bashrc" \
    "$BACKUP_PATH/.bash_profile" \
    "$BACKUP_PATH/.bash_aliases" \
    "$BACKUP_PATH/.vimrc" \
-   "$BACKUP_PATH/.i3status.conf" \
    "$HOME/"
-mv "$BACKUP_PATH/sway" "$XDG_CONFIG_HOME/sway/config"
 
 # Remove this script
 rm "$BACKUP_PATH/uninstall.sh"
