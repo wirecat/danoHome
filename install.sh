ede32479adbf083699a626dbb8dd4ddbd3d50a77
Daniel Johnson <djwirecat@gmail.com>
2018-09-08 11:35:15 -0700

    Fixes #3 by removing uninstall script.
    Can't have uninstall issues if you don't support uninstalling. I
    never plan to not use these configuration files, so I am going to
    stop supporting uninstall.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
 BACKUP_PATH="$HOME/.dano-home-backup"
 
 # If a backup folder already exists, then ERROR
 if [ -d "$BACKUP_PATH" ]; then
-   echo "ERROR: A backup folder already exists at \"$BACKUP_PATH\". \
-         Run the uninstall script located there first." 
+   echo "ERROR: A backup folder already exists at '$BACKUP_PATH'."
    exit 1
 fi
 
@@ -20,7 +20,3 @@ ln -s "$PWD/config-files/bashrc" "$HOME/.bashrc"
 ln -s "$PWD/config-files/bash_profile" "$HOME/.bash_profile"
 ln -s "$PWD/config-files/bash_aliases" "$HOME/.bash_aliases"
 ln -s "$PWD/config-files/vimrc" "$HOME/.vimrc"
-
-# Create a copy of the uninstall script in the backup directory in case someone
-# nukes this repo, but needs to recover.
-cp "$PWD/uninstall.sh" "$HOME/.dano-home-backup"
