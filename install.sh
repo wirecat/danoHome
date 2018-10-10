d56e78fd2a5c51e34f6876109df5e99435f4b555
Daniel Johnson <djwirecat@gmail.com>
2018-10-09 23:21:10 -0700

    Adds a compton configuration that supports transparency

--- install.sh
+++ install.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 BACKUP_PATH="$HOME/.dano-home-backup"
 
-# If a backup folder already exists, then ERROR
-if [ -d "$BACKUP_PATH" ]; then
-   echo "ERROR: A backup folder already exists at '$BACKUP_PATH'."
-   exit 1
-fi
-
 # Backup all affected files
 mkdir -p "$BACKUP_PATH"
 mv "$HOME/.bashrc" \
    "$HOME/.bash_profile" \
    "$HOME/.bash_aliases" \
    "$HOME/.vimrc" \
+   "$HOME/.config/compton.conf" \
    "$BACKUP_PATH" 2> /dev/null
 
 # Create symbolic links to the config files specified in this repository.
@@ -20,3 +15,4 @@ ln -s "$PWD/config-files/bashrc" "$HOME/.bashrc"
 ln -s "$PWD/config-files/bash_profile" "$HOME/.bash_profile"
 ln -s "$PWD/config-files/bash_aliases" "$HOME/.bash_aliases"
 ln -s "$PWD/config-files/vimrc" "$HOME/.vimrc"
+ln -s "$PWD/config-files/compton" "$HOME/.config/compton.conf"
